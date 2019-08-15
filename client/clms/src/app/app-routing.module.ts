cfe8ff05c00a32d4a57f376d23e832b6213d490c
Joe-Sendra <joe.sendra@gmail.com>
2019-08-15 15:48:45 -0500

    Route home and login components

--- client/clms/src/app/app-routing.module.ts
+++ client/clms/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './main-app/components/home/home.component';
+import { LoginComponent } from './shared/components/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
