04496b59a5f8e918e1ee1a179507a068ee079709
Joe-Sendra <joe.sendra@gmail.com>
2019-08-30 02:14:52 -0500

    Fixed role authorization bug on backend

--- client/clms/Backend/middleware/check-role.js
+++ client/clms/Backend/middleware/check-role.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     req.role = decodedToken.role;
     if (req.role !== 'Admin') {
-      res.status(401).json({ message: 'You are not authorized, must be an administrator!'});
+      return res.status(401).json({ message: 'You are not authorized, must be an administrator!'});
     }
     next();
   } catch (error) {
